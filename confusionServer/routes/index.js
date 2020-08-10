6c077ef65a8e1258887a5be386266bb0d01ae32b
Muhammad Awais <awaisshaikh94@gmail.com>
2020-08-10 06:14:12 -0700

    assignment 4

--- confusionServer/routes/index.js
+++ confusionServer/routes/index.js
@@ -6,6 +6,7 @@ const usersRouter = require('./users');
 const dishRouter = require('./dishRouter');
 const promoRouter = require('./promotionsRouter');
 const leaderRouter = require('./leaderRouter');
+const favoriteRouter = require('./favoriteRouter');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,5 +17,6 @@ router.use('/users', usersRouter);
 router.use('/dishes', dishRouter);
 router.use('/promotions', promoRouter);
 router.use('/leaders', leaderRouter);
+router.use('/favorites', favoriteRouter);
 
 module.exports = router;
